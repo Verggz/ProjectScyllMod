b9dc84c22553943fd88534fb247dfe2d6a693ac6
BuildTools <unconfigured@null.spigotmc.org>
2021-09-18 14:17:42 +0900

    added binflip and ahflip command

--- index.js
+++ index.js
@@ -21,6 +21,52 @@ function nFormatter(num, digits) {
   }
 
 register("command", () => SettingsUI.openGUI()).setName("settings");
+register("command", () => {
+  request({
+    'method':'POST',
+      url: 'https://projectscyllbin.herokuapp.com/api/v1/nitric/binsnipe/advise',
+      body:{
+        'profit': parseInt(SettingsStore.binrange)
+      },
+      json: true,
+      })
+      .then(function(response) {
+        bincall = false;
+        var clickableMessage = new Message(
+          `&3found a BIN snipe! (with command)` ,
+          new TextComponent("&3&l(Hover for more details) ").setHoverValue(`Item: ${response.random.item.item}, Usual price: ${nFormatter(response.random.usual.price,2)}, Auction price: ${nFormatter(response.random.item.price,2)}, Profit: ${nFormatter(response.random.usual.price - response.random.item.price,2)}`),
+           new TextComponent("&b&l&oclick here!").setClick("run_command", "/viewauction " + response.random.item.aucid),
+        );
+          ChatLib.chat(clickableMessage)
+      }).catch(function(e){
+        bincall = false;
+        console.log(e);
+    });
+}).setName("binflip");
+register("command", () => {
+  request({
+    'method':'POST',
+      url: 'https://projectscyll.herokuapp.com/api/v1/nitric/auction/advise/profit/rt',
+      body:{
+        'profit': parseInt(SettingsStore.ahrange)
+      },
+      json: true,
+      })
+      .then(function(response) {
+        ahcall = false;
+        var clickableMessage = new Message(
+          `&6found an AH flip! ` ,
+          new TextComponent("&6&l(Hover for more details) ").setHoverValue(`Item: ${response.item.item}, Usual price: ${nFormatter(response.item.normalprice)}, Auction price: ${nFormatter(response.item.curprice)}, Profit: ${nFormatter(response.item.profit)}`),
+           new TextComponent("&e&l&oclick here!").setClick("run_command", "/viewauction " + response.item.aucid),
+        );
+          ChatLib.chat(clickableMessage)
+      }).catch(function(e){
+        ahcall = false;
+        console.log(e);
+      });
+}).setName("ahflip");
+
+
 var guiKeyBind = getKeyBindFromKey(Keyboard.KEY_COMMA,"Open project scyll Settings");
 
 function getKeyBindFromKey(key, description) {
